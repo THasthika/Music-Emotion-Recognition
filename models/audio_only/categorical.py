6105f3db8a2b747befadeaaabae2996eb5a2c11a
Tharindu Hasthika <tharindubathigama@gmail.com>
2021-04-23 01:32:51 +0530

    base model implemented

--- models/audio_only/categorical.py
+++ models/audio_only/categorical.py
@@ -1,19 +1,14 @@
-from os import path
-
-import wandb
-
 import pytorch_lightning as pl
 
-from data import DeamDataset, MERTaffcDataset
+from models.base import BaseModel
 
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
 
 import torchmetrics as tm
 
-class ModelCatA(pl.LightningModule):
+class ModelCatA(BaseModel):
 
     CMDS = [
         ('lr', float, 0.001),
@@ -38,14 +33,9 @@ class ModelCatA(pl.LightningModule):
             conv1_kernel_stride
             adaptive_layer
         """
-        super().__init__()
+        super().__init__(batch_size=batch_size, num_workers=num_workers, data_artifact=data_artifact, split_artifact=split_artifact)
         self.config = config
         self.lr = config['lr']
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-
-        self.data_artifact = data_artifact
-        self.split_artifact = split_artifact
 
         ## build layers
         self.__build()
@@ -130,57 +120,3 @@ class ModelCatA(pl.LightningModule):
 
         self.log("test/loss", loss)
         self.log("test/acc", self.test_acc(pred, y))
-
-    def prepare_data(self):
-        split_at = wandb.use_artifact(self.split_artifact, type="data-split")
-        split_dir = split_at.download()
-
-        data_at = wandb.use_artifact(self.data_artifact, type="data-raw")
-        data_dir = data_at.download()
-
-        audio_dir = path.join(data_dir, "audio")
-        train_meta_file = path.join(split_dir, "train.json")
-        val_meta_file = path.join(split_dir, "val.json")
-        test_meta_file = path.join(split_dir, "test.json")
-
-        has_val = False
-        has_test = False
-        if path.exists(val_meta_file):
-            has_val = True
-        if path.exists(test_meta_file):
-            has_test = True
-
-        self.train_ds = None
-        self.val_ds = None
-        self.test_ds = None
-
-        DSClass = None
-        if self.data_artifact.startswith("mer-taffc"):
-            DSClass = MERTaffcDataset
-        elif self.data_artifact.startswith("deam"):
-            DSClass = DeamDataset
-
-        self.train_ds = DSClass(
-            audio_dir=audio_dir,
-            meta_file=train_meta_file)
-
-        if has_val:
-            self.val_ds = DSClass(
-                audio_dir=audio_dir,
-                meta_file=val_meta_file)
-
-        if has_test:
-            self.test_ds = DSClass(
-                audio_dir=audio_dir,
-                meta_file=test_meta_file)
-
-    def train_dataloader(self):
-        return DataLoader(self.train_ds, batch_size=self.batch_size, num_workers=self.num_workers)
-
-    def val_dataloader(self):
-        if self.val_ds is None: return None
-        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=self.num_workers)
-
-    def test_dataloader(self):
-        if self.test_ds is None: return None
-        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=self.num_workers)
---
+++ models/base.py
@@ -0,0 +1,82 @@
+from os import path
+
+import wandb
+
+import pytorch_lightning as pl
+
+from data import DeamDataset, MERTaffcDataset
+
+from torch.utils.data import DataLoader
+
+class BaseModel(pl.LightningModule):
+
+    def __init__(self, batch_size=32, num_workers=4, data_artifact=None, split_artifact=None):
+        """[summary]
+
+        Args:
+            batch_size (int, optional): [description]. Defaults to 32.
+            num_workers (int, optional): [description]. Defaults to 4.
+            data_artifact (str, optional): [description]. Defaults to None.
+            split_artifact (str, optional): [description]. Defaults to None.
+        """
+        super().__init__()
+
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.data_artifact = data_artifact
+        self.split_artifact = split_artifact
+
+    def prepare_data(self):
+        split_at = wandb.use_artifact(self.split_artifact, type="data-split")
+        split_dir = split_at.download()
+
+        data_at = wandb.use_artifact(self.data_artifact, type="data-raw")
+        data_dir = data_at.download()
+
+        audio_dir = path.join(data_dir, "audio")
+        train_meta_file = path.join(split_dir, "train.json")
+        val_meta_file = path.join(split_dir, "val.json")
+        test_meta_file = path.join(split_dir, "test.json")
+
+        has_val = False
+        has_test = False
+        if path.exists(val_meta_file):
+            has_val = True
+        if path.exists(test_meta_file):
+            has_test = True
+
+        self.train_ds = None
+        self.val_ds = None
+        self.test_ds = None
+
+        DSClass = None
+        if self.data_artifact.startswith("mer-taffc"):
+            DSClass = MERTaffcDataset
+        elif self.data_artifact.startswith("deam"):
+            DSClass = DeamDataset
+
+        self.train_ds = DSClass(
+            audio_dir=audio_dir,
+            meta_file=train_meta_file)
+
+        if has_val:
+            self.val_ds = DSClass(
+                audio_dir=audio_dir,
+                meta_file=val_meta_file)
+
+        if has_test:
+            self.test_ds = DSClass(
+                audio_dir=audio_dir,
+                meta_file=test_meta_file)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        if self.val_ds is None: return None
+        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        if self.test_ds is None: return None
+        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=self.num_workers)
