94f44ba8420a5f54803aa53d12b2b40e5d62df80
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-17 22:56:28 +0530

    testing

--- test_exec.py
+++ test_exec.py
@@ -1,23 +1,27 @@
+from models.revamped.audio_1dconv_stat import Audio1DConvStat
 from models.revamped import Audio1DConvCat
-from data import BaseAudioOnlyChunkedDataset
+from data import BaseAudioOnlyChunkedDataset, AudioOnlyStaticAVValues
 
 from torch.utils.data import DataLoader
 
 import torchinfo
 
-model = Audio1DConvCat()
+model = Audio1DConvStat()
 
-# ds = BaseAudioOnlyChunkedDataset(
-#     "/storage/s3/splits/mer-taffc-kfold/train.json",
-#     "/storage/s3/raw/mer-taffc/audio/",
-#     temp_folder="/tmp/xxx"
-# )
+ds = AudioOnlyStaticAVValues(
+    "/storage/s3/splits/mer-taffc-kfold/train.json",
+    "/storage/s3/raw/mer-taffc/audio/",
+    temp_folder="/tmp/xxx"
+)
 
-# dl = DataLoader(ds, batch_size=32, num_workers=4)
+dl = DataLoader(ds, batch_size=2, num_workers=4)
 
-torchinfo.summary(model, input_size=(32, 1, 22050*10))
+torchinfo.summary(model, input_size=(1, 1, 22050*10))
 
-# for (X, y) in dl:
-#     (X, y) = (X.to("cuda"), y.to("cuda"))
-#     y_pred = model(X)
-#     print("OK!")
+for (X, y) in dl:
+    print(X.shape)
+    print(y.shape)
+    # (X, y) = (X.to("cuda"), y.to("cuda"))
+    # y_pred = model(X)
+    # print("OK!")
+    break
