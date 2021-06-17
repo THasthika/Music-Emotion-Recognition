71d97cd6d751ae0d7bfbe12440cfbdfa76a594df
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-17 13:48:47 +0530

    updated deps

--- install-dep.sh
+++ install-dep.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 # wandb
-pip3 install -qqq wandb
+pip3 install wandb
 
 # pytorch
-# pip3 install -qqq torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+# pip3 install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 # pytorch_lightning
-pip3 install -qqq pytorch-lightning
+pip3 install pytorch-lightning
 
 # torchmetrics
-pip3 install -qqq torchmetrics
+pip3 install torchmetrics
 
-# pip3 install -qqq torchaudio
+# pip3 install torchaudio
 
 # torchlibrosa
-pip3 install -qqq torchlibrosa
+pip3 install torchlibrosa
 
 # dotenv
-pip3 install -qqq python-dotenv
+pip3 install python-dotenv
 
 # torchinfo
-pip3 install -qqq torchinfo
+pip3 install torchinfo
 
 # nnaudio
-pip3 install -qqq nnAudio
+pip3 install nnAudio
 
 # essentia
-pip3 install -qqq essentia
+pip3 install essentia
 
 #pylrc
-pip3 install -qqq git+https://github.com/THasthika/pylrc.git@timestamp-search
+pip3 install git+https://github.com/THasthika/pylrc.git@timestamp-search
 
 # trainsformers
-pip3 install -qqq transformers
+pip3 install transformers
 
 # coolname
-pip3 install -qqq coolname
+pip3 install coolname
 
 # librosa
-pip3 install -qqq librosa
+pip3 install librosa
