34f344a31589a9da01cacba6fe02eb5ed494eb62
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-17 10:39:16 +0530

    colab deps update

--- install-colab-dep.sh
+++ install-colab-dep.sh
@@ -4,7 +4,8 @@
 pip install -qqq wandb
 
 # pytorch
-pip install -qqq torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+# pip install -qqq torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+# pip install -qqq torch
 
 # pytorch_lightning
 pip install -qqq pytorch-lightning
