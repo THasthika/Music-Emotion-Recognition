53b2b7a1857f1e96615e422c57329c61adefcbcb
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-17 13:48:25 +0530

    updated deps

--- install-dep.sh
+++ install-dep.sh
@@ -4,7 +4,7 @@
 pip3 install -qqq wandb
 
 # pytorch
-pip3 install -qqq torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+# pip3 install -qqq torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 # pytorch_lightning
 pip3 install -qqq pytorch-lightning
@@ -12,7 +12,7 @@ pip3 install -qqq pytorch-lightning
 # torchmetrics
 pip3 install -qqq torchmetrics
 
-pip3 install -qqq torchaudio
+# pip3 install -qqq torchaudio
 
 # torchlibrosa
 pip3 install -qqq torchlibrosa
