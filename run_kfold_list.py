ca080b2bb44019b39ea379df0cad798ab7900159
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-18 23:59:50 +0530

    modified runner

--- run_kfold_list.py
+++ run_kfold_list.py
@@ -3,6 +3,7 @@ import run_kfold
 flist = open("./run_list", mode="r").readlines()
 
 for config_file in flist:
+    config_file = config_file.strip()
     if config_file.endswith(".yaml"):
         print("Running {}...".format(config_file))
         run_kfold.run(config_file)
