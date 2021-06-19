ddd56efec053436230c57007e0177bcf7c3aced4
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-19 18:24:47 +0530

    sagemaker init update

--- init-sagemaker.sh
+++ init-sagemaker.sh
@@ -4,8 +4,8 @@ conda create -y -n mer python=3.8
 
 conda install -y -n mer pytorch torchvision torchaudio torchtext cpuonly -c pytorch-lts
 
-cd /home/ec2-user/SageMaker/mer_research/
-
 conda install -y -n mer -c anaconda ipykernel
 
 python -m ipykernel install --user --name=mer
+
+cp /home/ec2-user/efs/config/sagemaker-default.yaml /home/ec2-user/SageMaker/mer_research/default.yaml
