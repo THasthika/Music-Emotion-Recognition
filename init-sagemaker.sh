ac42e1ec36b9df95029e8138385e41635ba392a2
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-19 18:33:12 +0530

    sagemaker init update

--- init-sagemaker.sh
+++ init-sagemaker.sh
@@ -9,3 +9,5 @@ conda install -y -n mer -c anaconda ipykernel
 python -m ipykernel install --user --name=mer
 
 cp /home/ec2-user/efs/config/sagemaker-default.yaml /home/ec2-user/SageMaker/mer_research/default.yaml
+
+cp /home/ec2-user/efs/config/mer-kernel.json /home/ec2-user/.local/share/jupyter/kernels/mer/kernel.json
