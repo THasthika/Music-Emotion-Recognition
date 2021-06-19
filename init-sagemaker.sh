50ca2483c7bc23f60b35431f145fca8d591b3aa6
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-19 18:43:45 +0530

    sagemaker init update

--- init-sagemaker.sh
+++ init-sagemaker.sh
@@ -11,3 +11,5 @@ python -m ipykernel install --user --name=mer
 cp /home/ec2-user/efs/config/sagemaker-default.yaml /home/ec2-user/SageMaker/mer_research/default.yaml
 
 cp /home/ec2-user/efs/config/mer-kernel.json /home/ec2-user/.local/share/jupyter/kernels/mer/kernel.json
+
+ln -s /home/ec2-user/efs/ /home/ec2-user/SageMaker/efs
