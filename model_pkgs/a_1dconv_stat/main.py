6f2efc388cddf601334fe1fdf60c3f95688dd72d
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-23 21:40:21 +0530

    stat model

---
+++ model_pkgs/a_1dconv_stat/main.py
@@ -0,0 +1,211 @@
+from re import sub
+from dotenv import load_dotenv
+import argparse
+import multiprocessing
+from torch.utils.data.dataloader import DataLoader
+import torchinfo
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from pytorch_lightning.loggers.wandb import WandbLogger
+import torch.cuda
+
+from model import A1DConvCat as Model
+from kfold import CrossValidator
+from data import ModelDataset
+
+load_dotenv(verbose=True)
+
+
+def make_datasets(args):
+    data_folder = args['data_folder']
+    train_meta = args['train_meta']
+    validation_meta = args['validation_meta']
+    test_meta = args['test_meta']
+    temp_folder = args['temp_folder']
+    force_compute = args['force_compute']
+
+    sr = args['sr']
+    duration = args['duration']
+    overlap = args['overlap']
+    ext = args['ext']
+
+    train_ds = ModelDataset(train_meta, data_folder, temp_folder=temp_folder, chunk_duration=duration,
+                            overlap=overlap, force_compute=force_compute, sr=sr, audio_extension=ext)
+    test_ds = ModelDataset(test_meta, data_folder, temp_folder=temp_folder, chunk_duration=duration,
+                           overlap=overlap, force_compute=force_compute, sr=sr, audio_extension=ext)
+    validation_ds = None
+    if not validation_meta is None:
+        validation_ds = ModelDataset(train_meta, data_folder, temp_folder=temp_folder, chunk_duration=duration,
+                                     overlap=overlap, force_compute=force_compute, sr=sr, audio_extension=ext)
+    return (train_ds, test_ds, validation_ds)
+
+
+def make_model(args, train_ds, test_ds, validation_ds=None):
+    batch_size = args['batch_size']
+    num_workers = args['num_workers']
+
+    lr = args['lr']
+    adaptive_layer_units = args['adaptive_layer_units']
+    model_config = dict(
+        lr=lr,
+        adaptive_layer_units=adaptive_layer_units
+    )
+
+    model = Model(batch_size=batch_size, num_workers=num_workers,
+                  train_ds=train_ds, val_ds=validation_ds, test_ds=test_ds, **model_config)
+    return (model, model_config)
+
+
+def get_gpu_count():
+    if torch.cuda.is_available():
+        return -1
+    return None
+
+
+def get_num_workers():
+    return multiprocessing.cpu_count()
+
+
+def get_wandb_tags(args):
+    return [
+        'model:A1DConvCat',
+        'dataset:{}'.format(args['dataset'])
+    ]
+
+
+def train_kfold(args):
+    (train_ds, test_ds, _) = make_datasets(args)
+    (model, model_config) = make_model(args, None, None)
+
+    config = {
+        **model_config
+    }
+
+    cv = CrossValidator(
+        n_splits=args['kfold_k'],
+        stratify=False,
+        batch_size=args['batch_size'],
+        num_workers=args['num_workers'],
+        wandb_project_name="mer",
+        model_monitor=Model.MODEL_CHECKPOINT,
+        model_monitor_mode=Model.MODEL_CHECKPOINT_MODE,
+        early_stop_monitor=Model.EARLY_STOPPING,
+        early_stop_mode=Model.EARLY_STOPPING_MODE,
+        use_wandb=(not args['no_wandb']),
+        cv_dry_run=False,
+        wandb_tags=get_wandb_tags(args),
+        config=config,
+        gpus=get_gpu_count()
+    )
+
+    cv.fit(model, train_ds, test_ds)
+
+def check(model, train_ds, test_ds, validation_ds):
+
+    for ds in [train_ds, test_ds, validation_ds]:
+        if ds is None:
+            continue
+
+        dl = DataLoader(ds, batch_size=2, num_workers=2, drop_last=True)
+
+        for (X, _) in dl:
+            model(X)
+            break
+
+    print("Model: foward passes ok!")
+
+    print(torchinfo.summary(model, input_size=(2, 1, 22050*5)))
+
+def train(args):
+
+    if args['kfold']:
+        train_kfold(args)
+        return
+
+    (train_ds, test_ds, validation_ds) = make_datasets(args)
+    (model, model_config) = make_model(args, train_ds, test_ds, validation_ds)
+
+    if args['check']:
+        check(model, train_ds, test_ds, validation_ds)
+        return
+
+    config={
+        **model_config
+    }
+
+    model_callback = ModelCheckpoint(monitor=Model.MODEL_CHECKPOINT, mode=Model.MODEL_CHECKPOINT_MODE)
+    early_stop_callback = EarlyStopping(
+        monitor=Model.EARLY_STOPPING,
+        min_delta=0.00,
+        patience=10,
+        verbose=True,
+        mode=Model.EARLY_STOPPING_MODE
+    )
+
+    logger = None
+    if not args['no_wandb']:
+        logger = WandbLogger(
+            offline=False,
+            log_model=True,
+            project='mer',
+            job_type="train",
+            config=config,
+            tags=get_wandb_tags(args)
+        )
+
+    trainer = pl.Trainer(
+        logger=logger,
+        gpus=get_gpu_count(),
+        callbacks=[model_callback, early_stop_callback])
+
+    trainer.fit(model)
+
+    trainer.test(model)
+
+
+def main(in_args=None):
+    parser = argparse.ArgumentParser(prog="Model")
+
+    subparsers = parser.add_subparsers(help='sub programs')
+    subparser_train = subparsers.add_parser('train', help='train the model')
+    subparser_train.set_defaults(func=train)
+    subparser_train.add_argument(
+        '--num-workers', type=int, default=get_num_workers())
+    subparser_train.add_argument(
+        '--no-wandb', action='store_true', default=False)
+    subparser_train.add_argument('--kfold', action='store_true', default=False)
+    subparser_train.add_argument('--kfold-k', type=int, default=5)
+
+    model_args = subparser_train.add_argument_group('Model Arguments')
+    model_args.add_argument('--check', action='store_true', default=False)
+    model_args.add_argument('--lr', '--learning-rate',
+                            type=float, default=0.01)
+    model_args.add_argument('--adaptive-layer-units',
+                            type=int, default=128)
+    model_args.add_argument('--batch-size', type=int, default=32)
+
+    data_args = subparser_train.add_argument_group('Dataset Arguments')
+    data_args.add_argument('--dataset', type=str, required=True)
+    data_args.add_argument('--data-folder', type=str, required=True)
+    data_args.add_argument('--train-meta', type=str, required=True)
+    data_args.add_argument('--validation-meta', type=str,
+                           required=False, default=None)
+    data_args.add_argument('--test-meta', type=str, required=True)
+    data_args.add_argument('--temp-folder', type=str, required=True)
+    data_args.add_argument(
+        '--force-compute', action='store_true', default=False)
+
+    audio_args = subparser_train.add_argument_group('Audio Arguments')
+    audio_args.add_argument('--sr', '--sample-rate', type=int, default=22050)
+    audio_args.add_argument('--duration', type=float, default=5)
+    audio_args.add_argument('--overlap', type=float, default=2.5)
+    audio_args.add_argument('--ext', '--extention', type=str, default='mp3')
+
+    args = parser.parse_args(in_args)
+    args.func(vars(args))
+
+
+if __name__ == "__main__":
+    main()
---
+++ model_pkgs/a_1dconv_stat/model.py
@@ -0,0 +1,128 @@
+import pytorch_lightning as pl
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import torchmetrics as tm
+
+class A1DConvCat(pl.LightningModule):
+
+    LR = "lr"
+    ADAPTIVE_LAYER_UNITS = "adaptive_layer_units"
+
+    EARLY_STOPPING = "val/loss"
+    EARLY_STOPPING_MODE = "min"
+    MODEL_CHECKPOINT = "val/loss"
+    MODEL_CHECKPOINT_MODE = "min"
+
+    def __init__(self,
+                batch_size=32,
+                num_workers=4,
+                train_ds=None,
+                val_ds=None,
+                test_ds=None,
+                **model_config):
+        super().__init__()
+
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.train_ds = train_ds
+        self.val_ds = val_ds
+        self.test_ds = test_ds
+
+        self.config = model_config
+
+        self.__build_model()
+
+        ## loss
+        self.loss = F.l1_loss
+    
+    def __build_model(self):
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=250, kernel_size=1024, stride=256),
+            nn.BatchNorm1d(250),
+            nn.Dropout(),
+            nn.ReLU(),
+
+            nn.Conv1d(in_channels=250, out_channels=250, kernel_size=13, stride=5),
+            nn.BatchNorm1d(250),
+            nn.Dropout(),
+            nn.ReLU(),
+
+            nn.Conv1d(in_channels=250, out_channels=250, kernel_size=13, stride=5),
+            nn.BatchNorm1d(250),
+            nn.Dropout(),
+            nn.ReLU(),
+
+            nn.Conv1d(in_channels=250, out_channels=250, kernel_size=13, stride=5),
+            nn.BatchNorm1d(250),
+            nn.Dropout(),
+            nn.ReLU(),
+
+            nn.AdaptiveAvgPool1d(output_size=self.config[self.ADAPTIVE_LAYER_UNITS]),
+            nn.Dropout()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=self.config[self.ADAPTIVE_LAYER_UNITS]*250, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=4)
+        )
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.classifier(x)
+        return x
+
+    def predict(self, x):
+        x = self.forward(x)
+        return x
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config[self.LR])
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        pred = self(x)
+        loss = self.loss(pred, y)
+
+        self.log('train/loss', loss, prog_bar=True, on_step=False, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+
+        pred = self(x)
+        loss = self.loss(pred, y)
+        
+        self.log("val/loss", loss, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+
+        pred = self(x)
+        loss = self.loss(pred, y)
+
+        self.log("test/loss", loss)
+
+    def train_dataloader(self):
+        if self.test_ds is None: return None
+        return DataLoader(self.train_ds, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        if self.val_ds is None: return None
+        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        if self.test_ds is None: return None
+        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=self.num_workers)
