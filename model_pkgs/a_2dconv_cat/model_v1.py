d25dc185b6357160e57ac1a6d55a257980282ad7
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-27 20:52:07 +0530

    updated model

--- model_pkgs/a_2dconv_cat/model_v1.py
+++ model_pkgs/a_2dconv_cat/model_v1.py
@@ -68,14 +68,6 @@ class A2DConvCat_V1(pl.LightningModule):
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=7, stride=3),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
-            
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=3),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(),
-
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=7, stride=3),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(),
 
             nn.AdaptiveAvgPool2d(output_size=(
                 self.config[self.ADAPTIVE_LAYER_UNITS_0],
@@ -83,7 +75,7 @@ class A2DConvCat_V1(pl.LightningModule):
             ))
         )
 
-        input_size = (self.config[self.ADAPTIVE_LAYER_UNITS_0] * self.config[self.ADAPTIVE_LAYER_UNITS_1] * 128)
+        input_size = (self.config[self.ADAPTIVE_LAYER_UNITS_0] * self.config[self.ADAPTIVE_LAYER_UNITS_1] * 32)
 
         self.classifier = nn.Sequential(
             nn.Linear(in_features=input_size, out_features=512),
