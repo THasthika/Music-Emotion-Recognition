3cb346bac28c363a719110c86444d371f552195c
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-28 11:59:31 +0530

    model update

--- model_pkgs/a_2dconv_cat/model_v1.py
+++ model_pkgs/a_2dconv_cat/model_v1.py
@@ -62,26 +62,32 @@ class A2DConvCat_V1(pl.LightningModule):
         self.feature_extractor = nn.Sequential(
 
             nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(),
 
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
 
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
 
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(),
 
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3),
             nn.BatchNorm2d(num_features=256),
             nn.ReLU(),
             
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3),
             nn.BatchNorm2d(num_features=256),
             nn.ReLU(),
 
