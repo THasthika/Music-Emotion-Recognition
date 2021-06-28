056d66be9389dc4738a71ef8f75638a956c7c460
Tharindu Bathigama <tharindubathigama@gmail.com>
2021-06-28 12:08:12 +0530

    model update

--- model_pkgs/a_2dconv_cat/model_v1.py
+++ model_pkgs/a_2dconv_cat/model_v1.py
@@ -66,10 +66,10 @@ class A2DConvCat_V1(pl.LightningModule):
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(),
 
-            # nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1),
-            # nn.MaxPool2d(kernel_size=3),
-            # nn.BatchNorm2d(num_features=32),
-            # nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
 
             # nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
             # nn.MaxPool2d(kernel_size=3),
