2e8606db6ac67f13492aff6c5ed5d03e719b1307
Tharindu Hasthika <tharindubathigama@gmail.com>
2021-07-01 09:12:05 +0530

    model update

--- model_pkgs/ac_1dconv_stat/activation.py
+++ model_pkgs/ac_1dconv_stat/activation.py
@@ -3,7 +3,7 @@ from torch import Tensor
 
 class CustomELU(nn.ELU):
 
-    def __init__(self, alpha: float, inplace: bool) -> None:
+    def __init__(self, alpha=1, inplace=False) -> None:
         super().__init__(alpha=alpha, inplace=inplace)
     
     def forward(self, input: Tensor) -> Tensor:
