8487d90357f769515cb9e55cc82b7d83668da3b1
Tharindu Hasthika <tharindubathigama@gmail.com>
2021-07-08 22:03:20 +0530

    new update

--- models/1dconv/cat/acl/model_v1.py
+++ models/1dconv/cat/acl/model_v1.py
@@ -37,6 +37,8 @@ class ACL1DConvCat_V1(BaseCatModel):
         self.bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.bert_model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
         self.bert_model = self.bert_model.to(device)
+        for param in self.bert_model.parameters():
+            param.requires_grad = False
 
         f_bins = (self.config[self.N_FFT] // 2) + 1
 
