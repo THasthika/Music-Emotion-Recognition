a44af4d8fd9db76dde8b18e7d1ea01a413ffecc2
Tharindu Hasthika <tharindubathigama@gmail.com>
2021-08-16 18:35:07 +0530

    new update

---
+++ models/2dconv/d/a/model_v1.py
@@ -0,0 +1,105 @@
+from models import BaseStatModel
+import pytorch_lightning as pl
+
+import torch
+import torch.nn as nn
+
+class A2DConvD_V1(BaseStatModel):
+
+    ADAPTIVE_LAYER_UNITS_0 = "adaptive_layer_units_0"
+    ADAPTIVE_LAYER_UNITS_1 = "adaptive_layer_units_1"
+
+    def __init__(self,
+                batch_size=32,
+                num_workers=4,
+                train_ds=None,
+                val_ds=None,
+                test_ds=None,
+                **model_config):
+        super().__init__(batch_size, num_workers, train_ds, val_ds, test_ds, **model_config)
+
+        self.__build_model()
+    
+    def __build_model(self):
+
+        self.feature_1d_extractor = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=500, kernel_size=1024, stride=256),
+            nn.MaxPool1d(kernel_size=2),
+            nn.BatchNorm1d(num_features=500),
+            nn.ReLU(),
+        )
+
+        self.feature_2d_extractor = nn.Sequential(
+
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=16),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=256),
+            nn.Dropout2d(p=self.config[self.DROPOUT]),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=(1, 1)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(num_features=256),
+            nn.Dropout2d(p=self.config[self.DROPOUT]),
+            nn.ReLU(),
+
+            nn.AdaptiveAvgPool2d(output_size=(
+                self.config[self.ADAPTIVE_LAYER_UNITS_0],
+                self.config[self.ADAPTIVE_LAYER_UNITS_1]
+            ))
+        )
+
+        out_channels = 256
+        input_size = (self.config[self.ADAPTIVE_LAYER_UNITS_0] * self.config[self.ADAPTIVE_LAYER_UNITS_1] * out_channels)
+
+        self.fc0 = nn.Sequential(
+            nn.Linear(in_features=input_size, out_features=512),
+            nn.Dropout(p=self.config[self.DROPOUT]),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=128),
+            nn.ReLU()
+        )
+
+        self.fc_mean = nn.Sequential(
+            nn.Linear(in_features=128, out_features=2)
+        )
+
+        self.fc_std = nn.Sequential(
+            nn.Linear(in_features=128, out_features=2),
+            self._get_std_activation()
+        )
+
+    def forward(self, x):
+        
+        x = self.feature_1d_extractor(x)
+        x = torch.unsqueeze(x, dim=1)
+        x = self.feature_2d_extractor(x)
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.fc0(x)
+
+        x_mean = self.fc_mean(x)
+        x_std = self.fc_std(x)
+        x = torch.cat((x_mean, x_std), dim=1)
+        return x
+    
